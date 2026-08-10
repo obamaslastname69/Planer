d796a7d14fc41b1f3dc7352cb74bb9f7b37fda2b
obamaslastname69 <98952668+obamaslastname69@users.noreply.github.com>
2026-08-10 21:45:58 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 /* Deine persönlichen Einstellungen.
    Diese Datei wird bei Updates NICHT überschrieben — hier einmal eintragen, fertig. */
 
-window.PLANER_CLIENT_ID = "";
+window.PLANER_CLIENT_ID = "746363255529-a67p9oi92lhi73q5e4u4abkucbcj8lcu.apps.googleusercontent.com";
